b2be269cd4af4567ca7f0a7ced4228e2babbb9a9
Riley Hardy <54298858+rileyjhardy@users.noreply.github.com>
2024-03-18 20:03:58 -0500

    Useless spec

--- spec/deepgram_spec.rb
+++ spec/deepgram_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Deepgram do
   it "has a version number" do
     expect(Deepgram::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
